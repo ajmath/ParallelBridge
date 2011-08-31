67765e42d152e1105dc1fc85ead99bd360120bd8
Andrew Matheny <andrew.j.matheny@gmail.com>
2011-08-30 20:50:03 -0400

    Updated assembly info

--- ParallelBridge/Properties/AssemblyInfo.cs
+++ ParallelBridge/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mono.paralell")]
+[assembly: AssemblyTitle("ParallelBridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ultra Electronics, ProLogic")]
-[assembly: AssemblyProduct("mono.paralell")]
-[assembly: AssemblyCopyright("Copyright © Ultra Electronics, ProLogic 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParallelBridge")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
