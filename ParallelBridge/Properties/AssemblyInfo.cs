53152b0564358a92063733687c5cce5ae35fe216
Andrew Matheny <andrew.j.matheny@gmail.com>
2011-08-30 20:45:07 -0400

    initial commit

---
+++ ParallelBridge/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("mono.paralell")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ultra Electronics, ProLogic")]
+[assembly: AssemblyProduct("mono.paralell")]
+[assembly: AssemblyCopyright("Copyright © Ultra Electronics, ProLogic 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b5a8fd7-604c-4ce7-98c7-9b8b06abe2da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParallelBridge/Threading.Tasks/TaskCompletionSource.cs
@@ -0,0 +1,162 @@
+// 
+// TaskCompletionSource.cs
+//  
+// Authors:
+//       Jérémie "Garuma" Laval <jeremie.laval@gmail.com>
+//       Marek Safar <marek.safar@gmail.com>
+// 
+// Copyright (c) 2009 Jérémie "Garuma" Laval
+// Copyright 2011 Xamarin, Inc (http://www.xamarin.com)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Mono.Threading.Tasks
+{
+	public class TaskCompletionSource<TResult>
+	{
+		readonly Task<TResult> source;
+		SpinLock opLock = new SpinLock (false);
+
+		public TaskCompletionSource ()
+		{
+			source = new Task<TResult> (null);
+			source.SetupScheduler (TaskScheduler.Current);
+		}
+		
+		public TaskCompletionSource (object state)
+		{
+			source = new Task<TResult> (null, state);
+			source.SetupScheduler (TaskScheduler.Current);
+		}
+		
+		public TaskCompletionSource (TaskCreationOptions creationOptions)
+		{
+			source = new Task<TResult> (null, creationOptions);
+			source.SetupScheduler (TaskScheduler.Current);
+		}
+		
+		public TaskCompletionSource (object state, TaskCreationOptions creationOptions)
+		{
+			source = new Task<TResult> (null, state, creationOptions);
+			source.SetupScheduler (TaskScheduler.Current);
+		}
+		
+		public void SetCanceled ()
+		{
+			if (!ApplyOperation (source.CancelReal))
+				ThrowInvalidException ();
+		}
+		
+		public void SetException (Exception exception)
+		{
+			if (exception == null)
+				throw new ArgumentNullException ("exception");
+			
+			SetException (new Exception[] { exception });
+		}
+		
+		public void SetException (IEnumerable<Exception> exceptions)
+		{
+			if (!TrySetException (exceptions))
+				ThrowInvalidException ();
+		}
+		
+		public void SetResult (TResult result)
+		{
+			if (!ApplyOperation (() => source.Result = result))
+				ThrowInvalidException ();
+		}
+				
+		static void ThrowInvalidException ()
+		{
+			throw new InvalidOperationException ("The underlying Task is already in one of the three final states: RanToCompletion, Faulted, or Canceled.");
+		}
+		
+		public bool TrySetCanceled ()
+		{
+			return ApplyOperation (source.CancelReal);
+		}
+		
+		public bool TrySetException (Exception exception)
+		{
+			if (exception == null)
+				throw new ArgumentNullException ("exception");
+			
+			return TrySetException (new Exception[] { exception });
+		}
+		
+		public bool TrySetException (IEnumerable<Exception> exceptions)
+		{
+			if (exceptions == null)
+				throw new ArgumentNullException ("exceptions");
+
+			var aggregate = new AggregateException (exceptions);
+			if (aggregate.InnerExceptions.Count == 0)
+				throw new ArgumentNullException ("exceptions");
+			
+			return ApplyOperation (() => source.HandleGenericException (aggregate));
+		}
+		
+		public bool TrySetResult (TResult result)
+		{
+			return ApplyOperation (() => source.Result = result);
+		}
+				
+		bool ApplyOperation (Action action)
+		{
+			bool taken = false;
+			try {
+				opLock.Enter (ref taken);
+				if (CheckInvalidState ())
+					return false;
+			
+				source.Status = TaskStatus.Running;
+
+				if (action != null)
+					action ();
+
+				source.Finish ();
+			
+				return true;
+			} finally {
+				if (taken)
+					opLock.Exit ();
+			}
+		}
+		
+		bool CheckInvalidState ()
+		{
+			return source.Status == TaskStatus.RanToCompletion ||
+				   source.Status == TaskStatus.Faulted || 
+				   source.Status == TaskStatus.Canceled;
+					
+		}
+		
+		public Task<TResult> Task {
+			get {
+				return source;
+			}
+		}
+	}
+}
+
